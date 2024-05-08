da7a2c11dfd37d3b7fb70d1a4f549a8a022866f2
Rashmi Gupta <rashmi.gupta7018@gmail.com>
2024-05-08 12:10:53 +0530

    add myutil

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("hii go")
-	fmt.Println("hello golang")
+// 	var name string = "rashmi" ; 
+// var version ="22.22.3"
+// 	fmt.Println(version);
+// 	fmt.Println(name);
+// var money=false
 
+// fmt.Println(money); 
+// 
+
+var Public= "jii" ; 
+var private="hii"
+fmt.Println(private) 
+fmt.Println(Public)
 }
